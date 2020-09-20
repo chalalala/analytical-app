45d898c6c73ddd25638931446623e82e5b934ca8
chalalala <neko_hime1212@yahoo.com>
2020-09-21 00:22:16 +0700

    Add toggler

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import {Image, Row, Col, Navbar, Container, Nav, ListGroup} from 'react-bootstrap';
+import {Image, Row, Col, Navbar, Container, Nav, ListGroup, Button} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import NavbarToggle from 'react-bootstrap/esm/NavbarToggle';
 
@@ -12,8 +12,14 @@ class App extends Component {
         <div id="sidebar">
           <FontAwesomeIcon icon="coffee" inverse/>
 
-          <div onClick={hideSidebar}>Click me</div>
-          
+          <nav class="navbar navbar-dark">
+              <span class="navbar-toggler-icon ml-auto" 
+                style={{width:20}}
+                onClick={hideSidebar}
+                id="hideToggler">
+              </span> 
+          </nav>
+
           <div className="user-info">
             <h6 id="name">{user.name}</h6>
             <div id="contact">{user.contact}</div>
@@ -29,8 +35,17 @@ class App extends Component {
           </ListGroup>
         </div>
       
-        <Navbar bg="light" className="justify-content-between">
-          <Navbar className="mr-auto">
+
+        <Navbar variant="light" bg="light">
+          <Navbar>
+            <div class="navbar navbar-light bg-light">
+              <span class="navbar-toggler-icon" 
+                style={{width:20,display:'none'}}
+                onClick={showSidebar}
+                id="showToggler"
+              >
+              </span>
+            </div>
             <Nav.Link href="#">Calendar</Nav.Link>
             <Nav.Link href="#">Mail</Nav.Link>
             <Nav.Link href="#">Contact</Nav.Link>
@@ -38,9 +53,9 @@ class App extends Component {
             <Nav.Link href="#">Stars</Nav.Link>
           </Navbar>
 
-          <Navbar className="mr-auto">
+          <Navbar className="ml-auto">
               <Image src={user.avatar} roundedCircle className="small-avatar"/>
-              <Navbar.Text>
+              <Navbar.Text style={{padding:0,textAlign:"left"}}>
                 <h6 style={{marginBottom:0}}>{user.name}</h6>
                 <div>{user.status}</div>
               </Navbar.Text>
@@ -65,6 +80,13 @@ const user = {
 
 var hideSidebar = () => {
   document.getElementById("sidebar").style.display = "none";
+  document.getElementById("showToggler").style.display = "block";
+}
+
+var showSidebar = () => {
+  document.getElementById("sidebar").style.display = "block";
+  document.getElementById("showToggler").style.display = "none";
+  document.getElementById("hideToggler").style.display = "block";
 }
 
 export default App;
