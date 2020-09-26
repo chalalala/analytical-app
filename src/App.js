9d59fe76b5a75fd5f8d2971c95c53adfec79e96c
Trinh Thao Phuong <thaophuong.trinh.9@gmail.com>
2020-09-26 16:29:11 +0700

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import {Image, Row, Col, Navbar, Container, Nav, ListGroup} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import ReactDOM from 'react-dom';
+import {  BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 class App extends Component {
   render(){
     return (
+      <Router>
       <div className="App">
         <div id="sidebar">
-
           <nav className="navbar navbar-dark">
               <span class="navbar-toggler-icon ml-auto" 
                 onClick={hideSidebar}
@@ -24,14 +26,17 @@ class App extends Component {
           </div>
 
           <div id="ref">
-            <p style={{opacity:0.8,fontSize:14}}>APPLICATION</p>
-            <p className="navg">Dashboards</p>
-            <p className="navg">Analytics</p>
-            <p className="navg">Project</p>
-            <p className="navg">Calendar</p> 
+            <ul>
+              <li style={{opacity:0.8}}><Link to ="/" style={styles.linkStyle}>Dashboards</Link></li>
+              <li >
+                <Link to ="/analytic" style={styles.linkStyle}>Analytics</Link>
+              </li>
+              <li><Link to ="/project"style={styles.linkStyle}>Project</Link></li>
+              <li><Link to ="/calendar" style={styles.linkStyle}>Calendar</Link></li>
+            </ul>
           </div>
         </div>
-      
+
         <Navbar variant="light" bg="light">
           <Navbar>
             <div class="navbar navbar-light">
@@ -57,23 +62,22 @@ class App extends Component {
               </Navbar.Text>
           </Navbar>
         </Navbar>
+        <Switch>
+          <Route path="/analytic">
+            <Analytic />
+          </Route>
+          <Route path="/project">
+            <Project />
+          </Route>
+          <Route path="/calendar">
+            <Calendar />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
 
-        <div className="content show" id="Analytics">
-          <div className="graph-wrapper">
-            <Row>
-              <Col>
-                <h6>{graph.title}</h6>
-                <div class="subtitle">{graph.subtitle}</div>  
-              </Col>
-            </Row>
-            <Row>
-              <div className="graph">
-                <img src={graph.image}/>
-              </div>
-            </Row>
-          </div>
-
-          <Row>
+          {/* <Row>
             <Col lg="7" style={{textAlign:'left'}}>
               <p style={{marginLeft:50}}>How are your active users trending over time?</p>
             </Col>
@@ -81,18 +85,44 @@ class App extends Component {
             <Col lg="5" style={{textAlign:'left'}}>
               <p>What are your top devices?</p>
             </Col>
-          </Row>     
-        </div>
-
-        <div className="content" id="Dashboards">
-          <p>Nothing's here</p>
-        </div>   
-
+          </Row>      */}
       </div>
+      </Router>
     )
   }
 }
-
+function Home(){
+  return (
+    <div>
+    <p>Nothing's here</p>
+    </div> 
+  );
+}
+function Calendar(){
+  return <h2>Calendar</h2>
+}
+function Analytic(){
+  return (
+    <div className="content show" id="Analytics">
+      <div className="graph-wrapper">
+        <Row>
+          <Col>
+            <h6>{graph.title}</h6>
+            <div class="subtitle">{graph.subtitle}</div>  
+          </Col>
+        </Row>
+        <Row>
+          <div className="graph">
+            <img src={graph.image}/>
+          </div>
+        </Row>
+      </div>
+    </div>
+  );
+}
+function Project(){
+  return <h2>Project</h2>
+}
 const user = {
   name: 'Mai Doan',
   contact: 'maidt.bi9162@st.usth.edu.vn',
@@ -117,18 +147,12 @@ var showSidebar = () => {
   document.getElementById("hideToggler").style.display = "block";
 }
 
-var displayContent = () => {
-  var container = document.getElementById("ref");
-  var navigator = container.getElementsByClassName("navg");
-  for (var i=0;i<navigator.length;i++){
-    navigator[i].addEventListener("click",function(){
-      var current = document.querySelector("show");
-      current.className = current.className.replace(" show","");
-      document.getElementById(navigator[i].innerHTML).className += " show";
-    })
-  }
-}
-
-// displayContent();
-
 export default App;
+
+let styles = {
+  linkStyle:{
+    textDecoration: 'none', 
+    color: "white",
+    opacity: 0.8
+  }
+};
